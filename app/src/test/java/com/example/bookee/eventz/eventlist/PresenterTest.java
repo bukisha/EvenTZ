cb7e3b1c62ff0ae0b9d64672ea547f444557c8a9
Bukisha <bukisha@gmail.com>
2018-04-11 14:54:36 +0200

    Presenter test for eventList changed,first time spy usage and it works thank god!!!

--- app/src/test/java/com/example/bookee/eventz/eventlist/PresenterTest.java
+++ app/src/test/java/com/example/bookee/eventz/eventlist/PresenterTest.java
@@ -1,13 +1,16 @@
 package com.example.bookee.eventz.eventlist;
 
 import com.example.bookee.eventz.data.Event;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
 import java.util.ArrayList;
 
 public class PresenterTest {
@@ -22,7 +25,8 @@ public class PresenterTest {
     private MvpContract.Model modelMock;
     @Mock
     private MvpContract.FetchEventsForCategoryCallback modelCallbackMock;
-
+    @Spy
+    private MvpContract.FetchEventsForCategoryCallback modelCallbackSpy;
 
     @Before
     public void setUp() {
@@ -30,6 +34,7 @@ public class PresenterTest {
         tPresenter = new Presenter(viewMock, modelMock);
         tList = new ArrayList<>();
         tNamesList = new ArrayList<>();
+
     }
 
     @Test
@@ -45,25 +50,24 @@ public class PresenterTest {
     @Test
     public void shouldPopulateEventListActivity() {
         //Given
-        Mockito.doAnswer(new Answer<Void>() {
-            @Override
-            public Void answer(InvocationOnMock invocation) {
-                viewMock.populateEventListActivity(tNamesList);
-                return null;
-            }
-        }).when(modelCallbackMock).onSuccess(tList);
 
-        Mockito.doAnswer(new Answer<Void>() {
+        Mockito.doAnswer(new Answer() {
             @Override
-            public Void answer(InvocationOnMock invocation) {
-                MvpContract.FetchEventsForCategoryCallback callback = invocation.getArgument(1);
-                callback.onSuccess(tList);
+            public Object answer(InvocationOnMock invocation) {
+                MvpContract.FetchEventsForCategoryCallback callback = (MvpContract.FetchEventsForCategoryCallback) invocation.getArguments()[1];
+                String name=invocation.getArgument(0);
+                System.out.println("Called with arguments "+name );
+                System.out.println("Setting up Spy on callback which is passed to the method.........." );
+
+                modelCallbackSpy=Mockito.spy(callback);
+
+                modelCallbackSpy.onSuccess(tList);
                 return null;
             }
-        }).when(modelMock).fetchEventsForCategory(T_CATEGORY_NAME, modelCallbackMock);
+        }).when(modelMock).fetchEventsForCategory(Mockito.anyString(),Mockito.any(MvpContract.FetchEventsForCategoryCallback.class));
         //When
         tPresenter.fetchEventsForCategory(T_CATEGORY_NAME);
         //Then
-        Mockito.verify(viewMock).populateEventListActivity(tNamesList);
+        Mockito.verify(modelCallbackSpy).onSuccess(tList);
     }
 }
