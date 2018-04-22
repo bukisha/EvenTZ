761a3798ad0da236d0eac9d1e10eef288e9aa333
Bukisha <bukisha@gmail.com>
2018-04-22 18:24:48 +0200

    CardView list in HomeActivity how has icons and text

--- app/src/main/java/com/example/bookee/eventz/home/CategoryCardsAdapter.java
+++ app/src/main/java/com/example/bookee/eventz/home/CategoryCardsAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.bookee.eventz.R;
 import com.example.bookee.eventz.data.Category;
 
@@ -32,7 +33,7 @@ public class CategoryCardsAdapter extends RecyclerView.Adapter<CategoryCardsAdap
 
     @Override
     public void onBindViewHolder(CategoryNameViewHolder holder, int position) {
-           // Glide.with(context).load(R.drawable.).centerCrop().into(holder.categoryLogo);
+            Glide.with(context).load(getHomeItemLogo(categoryList.get(position).getName())).centerCrop().into(holder.categoryLogo);
             holder.categoryName.setText(categoryList.get(position).getName());
     }
 
@@ -55,7 +56,29 @@ public class CategoryCardsAdapter extends RecyclerView.Adapter<CategoryCardsAdap
         }
     }
     public int getHomeItemLogo(String itemName) {
-            //TODO switch case sa with category names so them ethod returns coresponding images
+             switch(itemName) {
+                 case "Music": return R.drawable.ic_music_cat;
+                 case "Business & Professional": return R.drawable.ic_business_cat;
+                 case "Food & Drink": return R.drawable.ic_food_cat;
+                 case "Community & Culture": return R.drawable.ic_culture_cat;
+                 case "Performing & Visual Arts": return R.drawable.ic_art_cat;
+                 case "Film, Media & Entertainment": return R.drawable.ic_film_cat;
+                 case "Sports & Fitness": return R.drawable.ic_sport_cat;
+                 case "Health & Wellness": return R.drawable.ic_health_cat;
+                 case "Science & Technology": return R.drawable.ic_science_cat;
+                 case "Travel & Outdoor": return R.drawable.ic_travel_cat;
+                 case "Charity & Causes": return R.drawable.ic_charity_cat;
+                 case "Religion & Spirituality": return R.drawable.ic_religion_cat;
+                 case "Family & Education": return R.drawable.ic_education_cat;
+                 case "Seasonal & Holiday": return R.drawable.ic_holiday_cat;
+                 case "Government & Politics": return R.drawable.ic_politics_cat;
+                 case "Fashion & Beauty": return R.drawable.ic_fashion_cat;
+                 case "Home & Lifestyle": return R.drawable.ic_lifestyle_cat;
+                 case "Auto, Boat & Air": return R.drawable.ic_auto_cat;
+                 case "Hobbies & Special Interest": return R.drawable.ic_hobby_cat;
+                 case "Other": return R.drawable.ic_otherl_cat;
+                 case "School Activities": return R.drawable.ic_school_cat;
+                 }
         return 0;
     }
 }
